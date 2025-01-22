3b0bc43d94fba25cf0a1857ccbfb2fd04bf1e050
akshaiRamesh269 <akshai.ramesh@abilytics.com>
2025-01-22 22:41:17 +0530

    Create backend.tf

---
+++ infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "nodeapp-terraform-remote-bucket"
+    encrypt = true
+    key     = "tf/add-aws-ecr-ecs-fargate/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
